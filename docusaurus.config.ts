1825ce558bb39ff4dd55859429e119b94119dd64
mohit.singh <mohit.singh@telekom-digital.com>
2025-11-03 20:25:40 +0100

    fix: fixed baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://orchest-doc.github.io', // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/orchest-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
